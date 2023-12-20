04398650bc0a820a86bc11e4c531a92bac9a9f96
Gilcecler Carneiro <gilcecler@gmail.com>
2023-12-20 10:42:02 -0300

    Bcript password

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,31 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
+	@Autowired PasswordEncoder passwordEncoder;
+		
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("ENCODE = " +  passwordEncoder.encode("123456"));
+		
+		boolean result = passwordEncoder.matches("123456", "$2a$10$wtATb6BVXocao8fBUiy0yOkKUxzknMvjjE8Bbur1V/V07xupwlVqC");
+		
+		System.out.println("RESULTADO " + result);
+		
+		
+		
 	}
 
 }
